79285cf7d284ad625848d719257836f19fd4815f
linchao <lxx@lcc.com>
2024-10-14 15:58:44 +0800

    验证

--- mock/main.ts
+++ mock/main.ts
@@ -2,40 +2,16 @@ import { MockMethod } from "vite-plugin-mock";
 
 export default [
     {
-        url: "/api/auth/login",
+        url: "/api/main/test",
         method: "post",
         response: ({ body }) => {
             return {
                 "code": 200,
                 "data": {
-                    "token": "token.token",
-                    "expire": Date.parse(new Date().toString()) / 1000 + 3600,
+                    "message": "OK",
                 },
             }
         }
     },
-    {
-        url: "/api/auth/logout",
-        method:"post",
-        response:({body}) => {
-            return {
-                "code": 200,
-                "data": "ok",
-            }
-        }
-    },
-    {
-        url: "/api/auth/check",
-        method:"post",
-        // timeout: 2500,
-        response:({body}) => {
-            return {
-                "code": 200,
-                "data": {
-                    "token": "to",
-                    "expire": Date.parse(new Date().toString()) / 1000 + 3600,
-                },
-            }
-        }
-    },
+
 ] as MockMethod[];
--- src/components/MenuMain.vue
+++ src/components/MenuMain.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { post } from "/src/utils/request"
+
+const test = () => {
+    post("/main/test").then((data) => {
+        console.log(data)
+    });
+}
 
 </script>
 
 <template>
 
     Home
+    <el-button type="danger" @click="test">CLICK</el-button>
 
 </template>
 
